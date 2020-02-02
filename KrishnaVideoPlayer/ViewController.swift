1c9d6d0e98a36d15c401f6bc650631c5745fbeef
krishnarjun <krishnarjun.banoth@gmail.com>
2020-02-02 11:47:36 +0530

    Initial Commit

---
+++ KrishnaVideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KrishnaVideoPlayer
+//
+//  Created by Krishnarjun Banoth on 02/02/20.
+//  Copyright © 2020 Krishnarjun Banoth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
